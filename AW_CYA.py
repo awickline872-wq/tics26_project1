b7ee3d9eea3a76e574295f7a66b6bca83427cc4c
awickline872-wq <awickline872@phm.k12.in.us>
2026-08-27 12:39:23 -0400

    start of the choose your own adventure project

---
+++ AW_CYA.py
@@ -0,0 +1,25 @@
+name = input("Enter your name:")
+print("Hello", name)
+print("you were on a treasure hunt but accidentally fell into a cave now you must collect enough Gems to escape the cave")
+print("You Stand up and see a dangerous Cave animal you have two choices to escape ")
+print("A. escape by climbing the rocks")
+print("B. escape by jumping into the water")
+Choice1 = input("What do you choose(A or B): ")
+
+PathOne = {
+    "A" : ["1)", "2)"],
+    "B" : [
+    
+    ],
+    "p3" : []
+
+}
+PathTwo ={
+    "p1" : [
+        ],
+    "p2" : [
+        
+        ],
+    "p3" : []
+
+}
